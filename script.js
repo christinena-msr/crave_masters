736ef2d7ddfe15dcce58b4b2a562f45467d431b9
crispysodium <christinena.316@gmail.com>
2020-01-22 20:19:10 -0800

    added foursquare API fetch request

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let test = '';
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer b-pGPNxMyoDcwwOM5GnbddhGPt5ke00ZQMkhRBJgLb5NCKllnb6ZV-hSeLEwECBvAg7REo7eUJoLfo61lqA_oQ6_qxSXI7ERhzM9wukQ5KDSO_6we4tjfq7dyFMjXnYx");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'no-cors',
+  redirect: 'follow'
+};
+
+fetch("https://api.yelp.com/v3/businesses/search?location=seattle", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
