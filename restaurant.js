8e7209732c045a31f982dfb7fe1c8e65c8bea49a
crispysodium <christinena.316@gmail.com>
2020-01-25 11:38:53 -0800

    modified restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,9 +1,6 @@
 
-//api keys
-const client_id = "Z4OJRQC5DTCQDWM2PRZMWSRAMHIUSOGPMQHA2TUZE2Q4G5CF";
-const client_secret = "0OS1RK2RK4HY1AFEAVJUKEQC35WCYALLBRZELKM2LDJMINHX";
-
-//api variables
+var client_id = "Z4OJRQC5DTCQDWM2PRZMWSRAMHIUSOGPMQHA2TUZE2Q4G5CF";
+var client_secret = "0OS1RK2RK4HY1AFEAVJUKEQC35WCYALLBRZELKM2LDJMINHX";
 var query = "";
 var lat = "47.61";
 var lon = "-122.33";
@@ -13,6 +10,8 @@ var category = "4bf58dd8d48988d17a941735";
 fetch(`https://api.foursquare.com/v2/venues/search?client_id=${client_id}&client_secret=${client_secret}&v=20180323&limit=10&ll=${lat},${lon}&categoryId=${category}&radius=40000`)
     .then(function(response) {
         // Code for handling API response
+        // console.log(response);
+        // console.log(response.json());
         return response.json();
         
     })
@@ -23,13 +22,11 @@ fetch(`https://api.foursquare.com/v2/venues/search?client_id=${client_id}&client
         console.log(json.response.venues[1].name);
         for (let i=0; i<10; i++) {
             var box = document.getElementById("results-box");
-
             // icon code
             var rank = document.createElement("div");
-            rank.setAttribute("class", "mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--span-2-tablet icon-box");
+            rank.setAttribute("class", "mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--span-2-tablet");
             rank.textContent = i + 1;
             box.appendChild(rank);
-
             // restaurant name & address
             var results = document.createElement("div");
             results.setAttribute("class", "mdc-layout-grid__cell--span-8-desktop mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--span-4-tablet");
@@ -41,7 +38,6 @@ fetch(`https://api.foursquare.com/v2/venues/search?client_id=${client_id}&client
             results.appendChild(restName);
             results.appendChild(address);
             box.appendChild(results);
-            
             // delivery button code
             var delivery = document.createElement("button");
             delivery.setAttribute("class", "mdc-layout-grid__cell--span-2-desktop mdc-button mdc-button--raised mdc-layout-grid__cell--span-4-phone mdc-layout-grid__cell--span-2-tablet");
